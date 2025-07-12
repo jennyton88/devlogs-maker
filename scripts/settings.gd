450c73524a24b75316c9e1c5ab57654ba8a327ab
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2025-07-12 13:04:43 -0400

    updating settings now changes the base repo

--- scripts/settings.gd
+++ scripts/settings.gd
@@ -20,10 +20,6 @@ extends MarginContainer
 @onready var error_message = $ErrorPopup/S7/VBC2/Message;
 
 
-func _ready():
-	pass
-
-
 func setup_settings() -> void:
 	var config = ConfigFile.new();
 	var error = config.load("user://config.cfg");
@@ -43,27 +39,34 @@ func setup_settings() -> void:
 	email.text = config.get_value("user_info", "user_email");
 
 
+func save_settings():
+	var config = ConfigFile.new();
+	var error = config.load("user://config.cfg");
+		
+		# allow user to restart setup_tokens
+	if error != OK:
+		set_error("%d ERROR\nFailed to load config file." % error);
+		return;
+	
+	config.set_value("repo_info", "repo_owner", repo_owner.text);
+	config.set_value("repo_info", "repo_name", repo_name.text);
+	config.set_value("repo_info", "repo_branch_update", repo_branch.text);
+	
+	config.set_value("repo_info", "content_path", content_path.text);
+	
+	var build_url = "https://api.github.com/repos/%s/%s/contents/%s" % [repo_owner.text, repo_name.text, content_path.text];
+	config.set_value("urls", "base_repo", build_url);
+	
+	config.set_value("user_info", "user_name", author.text);
+	config.set_value("user_info", "user_email", email.text);
+	
+	config.save("user://config.cfg");
+
+
 func _on_save_settings_pressed(save_settings: bool) -> void:
-	if (save_settings):
-		var config = ConfigFile.new();
-		var error = config.load("user://config.cfg");
-		
-			# allow user to restart setup_tokens
-		if error != OK:
-			set_error("%d ERROR\nFailed to load config file." % error);
-			return;
-		
-		config.set_value("repo_info", "repo_owner", repo_owner.text);
-		config.set_value("repo_info", "repo_name", repo_name.text);
-		config.set_value("repo_info", "repo_branch_update", repo_branch.text);
-		
-		config.set_value("repo_info", "content_path", content_path.text);
-		
-		config.set_value("user_info", "user_name", author.text);
-		config.set_value("user_info", "user_email", email.text);
-		
-		config.save("user://config.cfg");
-	else:
+	if (save_settings): # apply
+		save_settings();
+	else: # cancel
 		setup_settings();
 
 
