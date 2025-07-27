667218c2b111fde9b16052171b0ba27e6ecb648d
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2025-07-27 09:24:34 -0400

    create app_info class to store global enums

---
+++ scripts/app_info.gd
@@ -0,0 +1,26 @@
+class_name AppInfo
+
+enum MsgType {
+	RequireAction,
+	Notification,
+}
+
+enum ErrorType {
+	ConfigError,
+	HTTPError,
+}
+
+enum Month {
+	January = 1,
+	Feburary = 2,
+	March = 3,
+	April = 4,
+	May = 5,
+	June = 6,
+	July = 7,
+	August = 8,
+	September = 9,
+	October = 10,
+	November = 11,
+	December = 12,
+}
