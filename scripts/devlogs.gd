8d02b643d3de7c3d1e03d0db589cc30264a5894f
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2025-07-30 11:37:18 -0400

    move devlog list methods from devlogs to devlog list, add methods to connect signals

--- scripts/devlogs.gd
+++ scripts/devlogs.gd
@@ -4,10 +4,11 @@ extends MarginContainer
 @onready var menu_options = $HB/TextOptions;
 
 # Workspace Modules
-@onready var editor = $HB/VB/MC/Workspace/Editor;
 @onready var finalize = $HB/VB/MC/Workspace/Finalize;
-@onready var verify_user = $"HB/VB/MC/Workspace/Verify User";
+@onready var editor = $HB/VB/MC/Workspace/Editor;
 @onready var text_preview = $HB/VB/MC/Workspace/Preview/PostPreview;
+@onready var verify_user = $"HB/VB/MC/Workspace/Verify User";
+@onready var post_list = $"HB/VB/MC/Workspace/Devlogs List";
 @onready var settings = $HB/VB/MC/Workspace/Settings;
 
 # Import / Export
@@ -16,16 +17,13 @@ extends MarginContainer
 # Message Popup
 @onready var msg_popup = $HB/VB/MC/MsgPopup;
 
-# Post list
-@onready var post_list = $"HB/VB/MC/Workspace/Devlogs List";
 
 # Temporary Variables
-var edit_button_ref = null;
 var creation_date = "";
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	menu_options.get_devlogs.connect(_on_get_devlogs);
+	menu_options.get_devlogs.connect(post_list._on_get_devlogs);
 	menu_options.post_curr_text.connect(_on_post_curr_text);
 	
 	menu_options.clear_text.connect(_on_clear_text);
@@ -40,6 +38,15 @@ func _ready():
 	editor.startup(_on_update_preview);
 	finalize.startup(_on_text_changed_preview, _on_update_preview);
 	settings.startup();
+	
+	post_list.startup([
+		create_error_popup,
+		create_notif_popup,
+		create_popup,
+		disconnect_popup,
+		clear_post,
+		fill_in_details
+	]);
 	
 	verify_user.startup(
 		_on_enable_buttons, 
@@ -64,7 +71,7 @@ func _on_post_curr_text():
 		create_error_popup(error, AppInfo.ErrorType.ConfigError);
 		return;
 	
-	var msg_type = "Edited" if edit_button_ref != null else "Posted";
+	var msg_type = "Edited" if post_list.get_edit_ref() != null else "Posted";
 	
 	var body = {
 		"message": "%s devlog." % msg_type,
@@ -76,8 +83,9 @@ func _on_post_curr_text():
 		"branch": config.get_value("repo_info", "repo_branch_update")
 	};
 	
-	if (edit_button_ref != null):
-		body["sha"] = edit_button_ref.get_meta("sha");
+	var edit_ref = post_list.get_edit_ref();
+	if (edit_ref != null):
+		body["sha"] = edit_ref.get_meta("sha");
 	
 	body = JSON.stringify(body);
 	
@@ -107,147 +115,16 @@ func _on_post_curr_text():
 		create_error_popup(error, AppInfo.ErrorType.HTTPError);
 
 
-func _on_get_devlogs():
-	var amt_of_children = post_list.get_child_count();
-	if (amt_of_children > 1):
-		var children = post_list.get_children();
-		for i in range(amt_of_children - 1, 0, -1):
-			post_list.remove_child(children[i]);
-			children[i].queue_free();
-	
-	var config = ConfigFile.new();
-	var error = config.load("user://config.cfg");
-	
-	if error != OK:
-		create_error_popup(error, AppInfo.ErrorType.ConfigError);
-		return;
-	
-	var app_name = config.get_value("app_info", "app_name");
-	var auth_type = config.get_value("user_info", "user_token_type");
-	var user_token = config.get_value("user_info", "user_token");
-	
-	var fields = { 
-		"ref": config.get_value("repo_info", "repo_branch_update"),
-	};
-	
-	var queries = create_query_string_from_dict(fields);
-	
-	var headers = [
-		"User-Agent: " + app_name,
-		"Accept: application/vnd.github+json",
-		"Accept-Encoding: gzip, deflate",
-		"Authorization: " + auth_type + " " + user_token,
-	];
-	
-	var h_client = HTTPRequest.new();
-	add_child(h_client);
-	h_client.request_completed.connect(_on_http_get_posts_completed);
-	
-	var url = config.get_value("urls", "base_repo");
-	# TODO url stripping depending on type of content path
-	url = url.rstrip("/") + "?"; # [/text_files/ vs /text_files] redirected to main branch
-	url += queries;
-	
-	error = h_client.request(url, headers, HTTPClient.METHOD_GET);
-	
-	if (error != OK):
-		create_error_popup(error, AppInfo.ErrorType.HTTPError);
-
-
-func _on_edit_button_pressed(button):
-	var config = ConfigFile.new();
-	var error = config.load("user://config.cfg");
-	
-	if error != OK:
-		create_error_popup(error, AppInfo.ErrorType.ConfigError);
-		return;
-	
-	edit_button_ref = button;
-	
-	var app_name = config.get_value("app_info", "app_name");
-	
-	var headers = [
-		"User-Agent: " + app_name,
-		"Accept: text/plain",
-		"Accept-Encoding: gzip, deflate",
-	];
-	
-	var h_client = HTTPRequest.new();
-	add_child(h_client);
-	h_client.request_completed.connect(_on_http_download_text_completed);
-	
-	var url = button.get_meta("url");
-	
-	error = h_client.request(url, headers, HTTPClient.METHOD_GET);
-	
-	if (error != OK):
-		create_error_popup(error, AppInfo.ErrorType.HTTPError);
-		edit_button_ref = null;
-
-
-func _on_delete_button_pressed(log_entry_delete_button: Button):
-	msg_popup.create_popup(
-		"Are you sure you want to delete this post?",
-		{'yes': ["Delete Post", _on_serious_delete_button_pressed.bind(log_entry_delete_button)], 'no': ["Cancel", _on_hide_popup]},
-		AppInfo.MsgType.RequireAction
-	);
 
 
 
 
 
-func _on_serious_delete_button_pressed(log_entry_delete_button: Button):
-	var button_ref = log_entry_delete_button;
-	
-	msg_popup.exit();
-	
-	var config = ConfigFile.new();
-	var error = config.load("user://config.cfg");
-	
-	if error != OK:
-		create_error_popup(error, AppInfo.ErrorType.ConfigError);
-		return;
-	
-	var body = {
-		"message": "Deleted devlog.",
-		"committer": {
-			"name": config.get_value("user_info", "user_name"),
-			"email": config.get_value("user_info", "user_email"),
-		},
-		"sha": button_ref.get_meta("sha"),
-		"branch": config.get_value("repo_info", "repo_branch_update")
-	};
-	
-	body = JSON.stringify(body);
-	
-	var app_name = config.get_value("app_info", "app_name");
-	var auth_type = config.get_value("user_info", "user_token_type");
-	var user_token = config.get_value("user_info", "user_token");
-	
-	var headers = [
-		"User-Agent: " + app_name,
-		"Accept: application/vnd.github+json",
-		"Accept-Encoding: gzip, deflate",
-		"Authorization: " + auth_type + " " + user_token,
-		"Content-Type: application/json", 
-		"Content-Length: " + str(body.length()),
-	];
-	
-	var h_client = HTTPRequest.new();
-	add_child(h_client);
-	h_client.request_completed.connect(_on_http_delete_post_completed);
-	
-	var url = config.get_value("urls", "base_repo");
-	url += button_ref.get_meta("name");
-	
-	error = h_client.request(url, headers, HTTPClient.METHOD_DELETE, body);
-	
-	if (error != OK):
-		create_error_popup(error, AppInfo.ErrorType.HTTPError);
-	else:
-		if (edit_button_ref != null && (button_ref.get_meta("sha") == edit_button_ref.get_meta("sha"))):
-			clear_post();
-		button_ref.get_parent().queue_free(); # delete the log entry in the devlog list
+
+
+
+
+
 
 
 func _on_import_file():	
@@ -278,14 +155,15 @@ func _on_http_post_completed(result, response_code, _headers, body):
 		HTTPClient.RESPONSE_OK: # update post
 			r_msg += "Successfully edited!";
 			var info = response["content"];
-			if (edit_button_ref != null):
-				edit_button_ref.set_meta("sha", info["sha"]);
-				edit_button_ref = null;
+			var edit_ref = post_list.get_edit_ref();
+			if (edit_ref != null):
+				edit_ref.set_meta("sha", info["sha"]);
+				post_list.set_edit_ref(null);
 				clear_post();
 		HTTPClient.RESPONSE_CREATED: # new post
 			r_msg += "Successfully created!";
 			var info = response["content"];
-			create_post_info(info["name"], info["download_url"], info["sha"]);
+			post_list.create_post_info(info["name"], info["download_url"], info["sha"]);
 			clear_post();
 		_:
 			r_msg += "Not implemented!";
@@ -294,56 +172,6 @@ func _on_http_post_completed(result, response_code, _headers, body):
 	create_notif_popup(r_msg);
 
 
-func _on_http_get_posts_completed(result, response_code, _headers, body):
-	if (failed_checks(result, response_code)):
-		return;
-	
-	var response = convert_to_json(body);
-	
-	match response_code:
-		HTTPClient.RESPONSE_OK:
-			for post in response:
-				create_post_info(post["name"], post["download_url"], post["sha"]);
-		_:
-			create_notif_popup("%d\nNot implemented!" % response_code);
-			print(response);
-
-
-func _on_http_download_text_completed(result, response_code, _headers, body):
-	if (failed_checks(result, response_code)):
-		return;
-	
-	match response_code:
-		HTTPClient.RESPONSE_OK:
-			var downloaded_text = body.get_string_from_utf8();
-			check_format_text(downloaded_text);
-		_:
-			create_notif_popup("%d\nNot implemented!" % response_code);
-			
-			print(body.get_string_from_utf8());
-
-
-func _on_http_delete_post_completed(result, response_code, _headers, body):
-	if (failed_checks(result, response_code)):
-		return;
-		
-	var response = convert_to_json(body);
-	
-	var r_msg = "%d\n" % response_code;
-	
-	match response_code:
-		HTTPClient.RESPONSE_OK:
-			r_msg += "Successfully deleted!";
-		HTTPClient.RESPONSE_NOT_FOUND:
-			r_msg += "Not found!";
-		HTTPClient.RESPONSE_CONFLICT:
-			r_msg += "There was a conflict!";
-		HTTPClient.RESPONSE_UNPROCESSABLE_ENTITY:
-			r_msg += "Validation failed: %s" % [response_code, response["message"]];
-		_:
-			r_msg += "Not implemented!\n%s" % [response_code, response["message"]];
-	
-	create_notif_popup(r_msg);
 
 
 func file_selected(path: String):
@@ -410,10 +238,9 @@ func clear_post():
 	editor.clear_text();
 	
 	text_preview.text = "";
-	
 	creation_date = "";
 	
-	edit_button_ref = null;
+	post_list.set_edit_ref(null);
 
 
 
@@ -453,32 +280,6 @@ func convert_to_json(body):
 	json.parse(body.get_string_from_utf8());
 	
 	return json.get_data();
-
-
-func create_post_info(new_file_name: String, url: String, sha: String):
-	var container = HBoxContainer.new();
-	var button = Button.new();
-	var delete_button = Button.new();
-	var a_post = Label.new();
-	
-	container.add_child(a_post);
-	a_post.text = new_file_name;
-	a_post.size_flags_horizontal = Control.SIZE_EXPAND_FILL;
-	
-	container.add_child(button);
-	button.text = "Edit";
-	button.set_meta("url", url);
-	button.set_meta("sha", sha);
-	button.set_meta("name", new_file_name);
-	button.pressed.connect(_on_edit_button_pressed.bind(button));
-	
-	container.add_child(delete_button);
-	delete_button.text = "Delete";
-	delete_button.set_meta("name", new_file_name);
-	delete_button.set_meta("sha", sha);
-	delete_button.pressed.connect(_on_delete_button_pressed.bind(delete_button));
-	
-	post_list.add_child(container);
 
 
 func get_curr_formatted_date():
@@ -525,54 +326,6 @@ func create_query_string_from_dict(fields: Dictionary) -> String:
 	return query_string;
 
 
-func check_format_text(text_blob) -> void:
-	if (edit_button_ref != null):
-		if (edit_button_ref.has_meta("name")):
-			var curr_file_name = edit_button_ref.get_meta("name");
-			var file_type = check_file_name(curr_file_name);
-
-			if (file_type != ""):
-				match file_type:
-					"devlog":
-						var split_text = text_blob.rsplit("\n");
-				
-						creation_date = split_text[1];
-						finalize.set_post_title(split_text[2]);
-						finalize.set_post_summary(split_text[3]);
-					
-						var str_len = 0;
-						for i in range(4):
-							str_len += split_text[i].length();
-					
-						editor.set_text(text_blob.substr(str_len + 4, -1)); # 4 of \n
-						finalize.set_filename(curr_file_name);
-					
-						_on_update_preview();
-					"directory":
-						print("directory")
-						pass;
-					"project":
-						print("project")
-						pass;
-					_:
-						create_notif_popup("Not a recognizable file name!\nPlease edit a different file.");
-
-
-func check_file_name(curr_file_name: String) -> String:
-	var regex = RegEx.new();
-	regex.compile("^(\\d{4})_(\\d{2})_(\\d{2})");
-	var matches = regex.search(curr_file_name);
-
-	if (matches):
-		return "devlog";
-
-	if (curr_file_name == "directory.txt"):
-		return "directory";
-	
-	if (curr_file_name == "projects_info.txt"):
-		return "project";
-	
-	return "";
 
 
 func _on_hide_popup():
@@ -597,3 +350,25 @@ func create_error_popup(error_code: Error, error_type: AppInfo.ErrorType):
 			error_msg += "Couldn't perform HTTP request.";
 	
 	create_notif_popup(error_msg);
+
+
+func create_popup(msg_text: String, button_info: Dictionary, msg_type: AppInfo.MsgType):
+	msg_popup.create_popup(
+		msg_text,
+		button_info,
+		msg_type
+	);
+
+
+func fill_in_details(post_info: Dictionary):
+	creation_date = post_info["creation_date"];
+	finalize.set_post_title(post_info["post_title"]);
+	finalize.set_post_summary(post_info["post_summary"]);
+	editor.set_text(post_info["post_body"]);
+	finalize.set_filename(post_info["filename"]);
+	
+	_on_update_preview();
+
+
+func disconnect_popup():
+	msg_popup.exit();
---
+++ scripts/devlogs_list.gd
@@ -0,0 +1,330 @@
+extends MarginContainer
+
+signal create_error_popup(msg_text: String, err_type: AppInfo.ErrorType);
+signal create_notif_popup(msg_text: String);
+signal create_popup(msg_text: String, button_info: Dictionary, msg_type: AppInfo.MsgType);
+signal disconnect_popup;
+signal clear_post;
+signal fill_in_details(post_info: Dictionary);
+
+@onready var list = $ScrollContainer/List;
+
+
+var edit_button_ref = null;
+
+
+func startup(connections: Array):
+	create_error_popup.connect(connections[0]);
+	create_notif_popup.connect(connections[1]);
+	create_popup.connect(connections[2])
+	disconnect_popup.connect(connections[3]);
+	clear_post.connect(connections[4]);
+	fill_in_details.connect(connections[5]);
+
+
+
+func create_post_info(new_file_name: String, url: String, sha: String):
+	var container = HBoxContainer.new();
+	var button = Button.new();
+	var delete_button = Button.new();
+	var a_post = Label.new();
+	
+	container.add_child(a_post);
+	a_post.text = new_file_name;
+	a_post.size_flags_horizontal = Control.SIZE_EXPAND_FILL;
+	
+	container.add_child(button);
+	button.text = "Edit";
+	button.set_meta("url", url);
+	button.set_meta("sha", sha);
+	button.set_meta("name", new_file_name);
+	button.pressed.connect(_on_edit_button_pressed.bind(button));
+	
+	container.add_child(delete_button);
+	delete_button.text = "Delete";
+	delete_button.set_meta("name", new_file_name);
+	delete_button.set_meta("sha", sha);
+	delete_button.pressed.connect(_on_delete_button_pressed.bind(delete_button));
+	
+	list.add_child(container);
+
+
+func _on_get_devlogs():
+	var amt_of_children = list.get_child_count();
+	if (amt_of_children > 1):
+		var children = list.get_children();
+		for i in range(amt_of_children - 1, 0, -1):
+			list.remove_child(children[i]);
+			children[i].queue_free();
+	
+	var config = load_config_file();
+	
+	if (config == null):
+		return;
+	
+	var app_name = config.get_value("app_info", "app_name");
+	var auth_type = config.get_value("user_info", "user_token_type");
+	var user_token = config.get_value("user_info", "user_token");
+	
+	var fields = { 
+		"ref": config.get_value("repo_info", "repo_branch_update"),
+	};
+	
+	var queries = HTTPClient.new().query_string_from_dict(fields);
+	
+	var headers = [
+		"User-Agent: " + app_name,
+		"Accept: application/vnd.github+json",
+		"Accept-Encoding: gzip, deflate",
+		"Authorization: " + auth_type + " " + user_token,
+	];
+	
+	var h_client = HTTPRequest.new();
+	add_child(h_client);
+	h_client.request_completed.connect(_on_http_get_posts_completed);
+	
+	var url = config.get_value("urls", "base_repo");
+	# TODO url stripping depending on type of content path
+	url = url.rstrip("/") + "?"; # [/text_files/ vs /text_files] redirected to main branch
+	url += queries;
+	
+	var error = h_client.request(url, headers, HTTPClient.METHOD_GET);
+	
+	if (error != OK):
+		create_error_popup.emit(error, AppInfo.ErrorType.HTTPError);
+
+
+func _on_http_get_posts_completed(result, response_code, _headers, body):
+	if (failed_checks(result, response_code)):
+		return;
+	
+	var response = convert_to_json(body);
+	
+	match response_code:
+		HTTPClient.RESPONSE_OK:
+			for post in response:
+				create_post_info(post["name"], post["download_url"], post["sha"]);
+		_:
+			create_notif_popup.emit("%d\nNot implemented!" % response_code);
+			print(response);
+
+
+func _on_edit_button_pressed(button: Button):
+	var config = ConfigFile.new();
+	var error = config.load("user://config.cfg");
+	
+	if error != OK:
+		create_error_popup.emit(error, AppInfo.ErrorType.ConfigError);
+		return;
+	
+	edit_button_ref = button;
+	
+	var app_name = config.get_value("app_info", "app_name");
+	
+	var headers = [
+		"User-Agent: " + app_name,
+		"Accept: text/plain",
+		"Accept-Encoding: gzip, deflate",
+	];
+	
+	var h_client = HTTPRequest.new();
+	add_child(h_client);
+	h_client.request_completed.connect(_on_http_download_text_completed);
+	
+	var url = button.get_meta("url");
+	
+	error = h_client.request(url, headers, HTTPClient.METHOD_GET);
+	
+	if (error != OK):
+		create_error_popup.emit(error, AppInfo.ErrorType.HTTPError);
+		edit_button_ref = null;
+
+
+func _on_http_download_text_completed(result, response_code, _headers, body):
+	if (failed_checks(result, response_code)):
+		return;
+	
+	match response_code:
+		HTTPClient.RESPONSE_OK:
+			var downloaded_text = body.get_string_from_utf8();
+			check_format_text(downloaded_text);
+		_:
+			create_notif_popup.emit("%d\nNot implemented!" % response_code);
+			
+			print(body.get_string_from_utf8());
+
+
+func check_format_text(text_blob) -> void:
+	if (edit_button_ref != null):
+		if (edit_button_ref.has_meta("name")):
+			var curr_filename = edit_button_ref.get_meta("name");
+			var file_type = check_file_name(curr_filename);
+
+			if (file_type != ""):
+				match file_type:
+					"devlog":
+						var split_text = text_blob.rsplit("\n");
+						
+						var post_data = {
+							"filename": curr_filename,
+							"creation_date": split_text[1],
+							"post_title": split_text[2],
+							"post_summary": split_text[3]
+						};
+					
+						var str_len = 0;
+						for i in range(4):
+							str_len += split_text[i].length();
+					
+						post_data["post_body"] = text_blob.substr(str_len + 4, -1); # 4 of \n
+					
+						fill_in_details.emit(post_data);
+					"directory":
+						print("directory")
+						pass;
+					"project":
+						print("project")
+						pass;
+					_:
+						create_notif_popup.emit("Not a recognizable file name!\nPlease edit a different file.");
+
+
+func _on_delete_button_pressed(log_entry_delete_button: Button):
+	create_popup.emit(
+		"Are you sure you want to delete this post?",
+		{'yes': ["Delete Post", _on_serious_delete_button_pressed.bind(log_entry_delete_button)], 'no': ["Cancel", _on_hide_popup]},
+		AppInfo.MsgType.RequireAction
+	);
+
+
+func _on_serious_delete_button_pressed(log_entry_delete_button: Button):
+	var button_ref = log_entry_delete_button;
+	
+	disconnect_popup.emit();
+	
+	var config = ConfigFile.new();
+	var error = config.load("user://config.cfg");
+	
+	if error != OK:
+		create_error_popup.emit(error, AppInfo.ErrorType.ConfigError);
+		return;
+	
+	var body = {
+		"message": "Deleted devlog.",
+		"committer": {
+			"name": config.get_value("user_info", "user_name"),
+			"email": config.get_value("user_info", "user_email"),
+		},
+		"sha": button_ref.get_meta("sha"),
+		"branch": config.get_value("repo_info", "repo_branch_update")
+	};
+	
+	body = JSON.stringify(body);
+	
+	var app_name = config.get_value("app_info", "app_name");
+	var auth_type = config.get_value("user_info", "user_token_type");
+	var user_token = config.get_value("user_info", "user_token");
+	
+	var headers = [
+		"User-Agent: " + app_name,
+		"Accept: application/vnd.github+json",
+		"Accept-Encoding: gzip, deflate",
+		"Authorization: " + auth_type + " " + user_token,
+		"Content-Type: application/json", 
+		"Content-Length: " + str(body.length()),
+	];
+	
+	var h_client = HTTPRequest.new();
+	add_child(h_client);
+	h_client.request_completed.connect(_on_http_delete_post_completed);
+	
+	var url = config.get_value("urls", "base_repo");
+	url += button_ref.get_meta("name");
+	
+	error = h_client.request(url, headers, HTTPClient.METHOD_DELETE, body);
+	
+	if (error != OK):
+		create_error_popup.emit(error, AppInfo.ErrorType.HTTPError);
+	else:
+		if (edit_button_ref != null && (button_ref.get_meta("sha") == edit_button_ref.get_meta("sha"))):
+			clear_post.emit();
+		button_ref.get_parent().queue_free(); # delete the log entry in the devlog list
+
+
+
+func load_config_file() -> ConfigFile:
+	var config = ConfigFile.new();
+	var error = config.load("user://config.cfg");
+	
+	if error != OK:
+		create_error_popup.emit(error, AppInfo.ErrorType.ConfigError);
+		return null;
+	
+	return config;
+
+
+
+func failed_checks(result: int, response_code: int):
+	if (result != OK):
+		var error_result = "%d\nHTTP request response error.\nResult %d" % [response_code, result];
+		create_notif_popup.emit(error_result);
+		return true;
+
+
+func convert_to_json(body):
+	var json = JSON.new();
+	json.parse(body.get_string_from_utf8());
+	
+	return json.get_data();
+
+
+func check_file_name(curr_file_name: String) -> String:
+	var regex = RegEx.new();
+	regex.compile("^(\\d{4})_(\\d{2})_(\\d{2})");
+	var matches = regex.search(curr_file_name);
+
+	if (matches):
+		return "devlog";
+
+	if (curr_file_name == "directory.txt"):
+		return "directory";
+	
+	if (curr_file_name == "projects_info.txt"):
+		return "project";
+	
+	return "";
+
+
+func _on_hide_popup():
+	disconnect_popup.emit();
+
+
+func _on_http_delete_post_completed(result, response_code, _headers, body):
+	if (failed_checks(result, response_code)):
+		return;
+		
+	var response = convert_to_json(body);
+	
+	var r_msg = "%d\n" % response_code;
+	
+	match response_code:
+		HTTPClient.RESPONSE_OK:
+			r_msg += "Successfully deleted!";
+		HTTPClient.RESPONSE_NOT_FOUND:
+			r_msg += "Not found!";
+		HTTPClient.RESPONSE_CONFLICT:
+			r_msg += "There was a conflict!";
+		HTTPClient.RESPONSE_UNPROCESSABLE_ENTITY:
+			r_msg += "Validation failed: %s" % [response_code, response["message"]];
+		_:
+			r_msg += "Not implemented!\n%s" % [response_code, response["message"]];
+	
+	create_notif_popup.emit(r_msg);
+
+
+func get_edit_ref():
+	return edit_button_ref;
+
+
+func set_edit_ref(updated):
+	edit_button_ref = updated;
