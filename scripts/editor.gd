0c6eff528ead8f339626c1bac6141fb0b085d820
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2025-07-31 14:39:26 -0400

    add comments editor

--- scripts/editor.gd
+++ scripts/editor.gd
@@ -1,23 +1,40 @@
 extends MarginContainer
 
+# =====================
+# ======= Nodes =======
+# =====================
+
 @onready var text_edit = $EditText;
 
 
+# =====================
+# ====== Methods ======
+# =====================
+
 func startup(connect_method: Callable):
 	text_edit.text_changed.connect(connect_method);
+
+
+func clear_text():
+	text_edit.text = "";
 
 
 func text_is_empty():
 	return text_edit.text == "";
 
 
-func set_text(new_text: String):
-	text_edit.text = new_text;
-
+# =====================
+# ====== Getters ======
+# =====================
 
 func get_text():
 	return text_edit.text;
 
 
-func clear_text():
-	text_edit.text = "";
+# =====================
+# ====== Setters ======
+# =====================
+
+
+func set_text(new_text: String):
+	text_edit.text = new_text;
