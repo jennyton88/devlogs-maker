36c9a08e8d2c9fcf4592e123d6ef4514b7b327c1
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2025-09-03 13:43:16 -0400

    moved image script to correct location, add more labels, delete button, checkmark for image selection

--- scripts/images.gd
+++ scripts/images.gd
@@ -2,9 +2,12 @@ extends MarginContainer
 
 @onready var img_list = $Scroll/VBox;
 
+var curr_img_num = 0;
 
 func save_img(img_data, img_name):
 	img_list.add_child(build_img_part(img_data, img_name));
+	
+	curr_img_num += 1;
 
 
 func build_img_part(img_data, img_name):
@@ -14,6 +17,12 @@ func build_img_part(img_data, img_name):
 	panel_cont.add_theme_stylebox_override("panel", bg_mat);
 	
 	var hbox = HBoxContainer.new();
+	
+	var num_label = Label.new();
+	num_label.text = "%d" % curr_img_num;
+	
+	var check = CheckBox.new();
+	check.size_flags_vertical = Control.SIZE_SHRINK_CENTER;
 	
 	var thumb = TextureRect.new();
 	thumb.expand_mode = TextureRect.EXPAND_FIT_WIDTH_PROPORTIONAL;
@@ -26,8 +35,20 @@ func build_img_part(img_data, img_name):
 	filename.size_flags_horizontal = Control.SIZE_EXPAND_FILL;
 	filename.text = img_name;
 	
+	var delete_button = Button.new();
+	delete_button.text = "Delete";
+	delete_button.pressed.connect(_on_delete_button_pressed.bind(delete_button));
+	
 	panel_cont.add_child(hbox);
+	hbox.add_child(num_label);
+	hbox.add_child(check);
 	hbox.add_child(thumb);
 	hbox.add_child(filename);
+	hbox.add_child(delete_button);
+	hbox.add_child(MarginContainer.new());
 	
 	return panel_cont;
+
+
+func _on_delete_button_pressed(delete_button):
+	delete_button.get_parent().queue_free();
