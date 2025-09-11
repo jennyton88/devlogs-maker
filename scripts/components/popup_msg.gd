646873eefbf66e66f3544caebe7a0d2a9da8e346
Jenny Ton <143558707+jennyton88@users.noreply.github.com>
2025-09-11 18:50:24 -0400

    popup will be instantiated everytime there is a new notification instead of reusing the same one

--- scripts/components/popup_msg.gd
+++ scripts/components/popup_msg.gd
@@ -1,15 +1,7 @@
 extends MarginContainer
 
-signal send_buttons(type: String, buttons: Dictionary);
 
-var yes_callable = null;
-var no_callable = null;
-
-func create_popup(
-	display_text: String, 
-	button_info: Dictionary,
-	type: AppInfo.MsgType,
-) -> void:
+func create_popup(display_text: String, button_info: Dictionary, type: AppInfo.MsgType):
 	set_msg(display_text);
 	setup_popup(type, button_info);
 
@@ -23,42 +15,30 @@ func setup_popup(type: AppInfo.MsgType, button_info: Dictionary) -> void:
 	var yes_action = get_node('Space/VB/HB/Yes');
 	var no_action = get_node('Space/VB/HB/No');
 	
+	
 	match type:
 		AppInfo.MsgType.Notification:
-			send_buttons.emit(type, {'yes': yes_action});
-			
-			yes_action.text = button_info['yes'][0];
-			
-			yes_action.pressed.connect(button_info['yes'][1]);
-			yes_callable = button_info['yes'][1];
+			yes_action.text = button_info['yes']['text'];
+			yes_action.pressed.connect(_on_yes_action.bind(button_info['yes']['action']));
 			
 			no_action.hide();
 		AppInfo.MsgType.RequireAction:
-			send_buttons.emit(type, {'yes': yes_action, 'no': no_action});
+			yes_action.text = button_info['yes']['text'];
+			no_action.text = button_info['no']['text'];
 			
-			yes_action.text = button_info['yes'][0];
-			no_action.text = button_info['no'][0];
-			
-			yes_action.pressed.connect(button_info['yes'][1]);
-			yes_callable = button_info['yes'][1];
-			no_action.pressed.connect(button_info['no'][1]);
-			no_callable = button_info['no'][1];
+			yes_action.pressed.connect(_on_yes_action.bind(button_info['yes']['action']));
+			no_action.pressed.connect(exit);
 			
 			no_action.show();
 	
 	show();
 
 
+func _on_yes_action(action: Callable):
+	action.call();
+	exit();
+
+
 func exit() -> void:
-	var yes_action = get_node('Space/VB/HB/Yes');
-	var no_action = get_node('Space/VB/HB/No');
-	
-	if (yes_callable):
-		yes_action.disconnect("pressed", yes_callable);
-		yes_callable = null;
-	
-	if (no_callable):
-		no_action.disconnect("pressed", no_callable);
-		no_callable = null;
-	
 	hide();
+	queue_free();
